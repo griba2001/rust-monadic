25d3c0df66b2b7f75410ae3dd3f6dfce628c8e83
Gabriel <griba2001@gmail.com>
2019-11-15 12:41:16 +0100

    comments

--- monadic/src/lib.rs
+++ monadic/src/lib.rs
@@ -1,33 +1,52 @@
 //!
-//! haskell style "monadic" macro 
-//! where monad sources should be collections implementing IntoIterator 
-//! consuming its elements to build the result
-//! and flat_mapping at each instruction
+//! Haskell style "monadic" macro 
+//! where monad sources should be expressions implementing IntoIterator 
 //!
-//! You can use 
-//! ```Some( returnValue)```       a monadic return
-//! ```v <- monadic_expression```  using the monad result
-//! ```_ <- monadic_expression```  ignoring the monad result
-//! ```let z = expression```       combining monad results
-//! ```guard boolean_expression``` to filter results
+//! Each step monad expression is flat_mapped with the rest into a lazy FlatMap expression which implements IntoIterator
 //!
+//! ```
+//! # #[macro_use] extern crate monadic;
+//!
+//! # fn main() {
+//!    let xs = monadic!{ 
+//!        x <- 1..5;
+//!        y <- 1..x;
+//!        guard y < x;
+//!        let z = y - 1;
+//!        Some((x,z)) 
+//!    }.collect::<Vec<(i32,i32)>>();
+//!    
+//!    println!("result: {:?}", xs); 
+//! # }
+//!
+//!  // test:
+//!
+//!  fn it_works() {
+//!        let xs = (1..5).collect::<Vec<i32>>();
+//!        // expected
+//!        let zs = (&xs).into_iter().filter(|&v| v < &4).map(|v| v*2).collect::<Vec<i32>>();
+//!        // monadic
+//!        let ys = monadic!{
+//!           v <- &xs;
+//!           guard v < &4;
+//!           Some( v * 2)
+//!        }.collect::<Vec<i32>>();
+//!        
+//!        assert_eq!(ys, zs);
+//!    }
+//! ```
 
 
-/// ```
-/// # #[macro_use] extern crate monadic;
+
+/// monadic chaining (with flat_map) of IntoIterator's as monads
 ///
-/// # fn main() {
-///    let xs = monadic!{ 
-///        x <- 1..5;
-///        y <- 1..=x;
-///        guard y < x;
-///        let z = y - 1;
-///        Some((x,z)) 
-///    }.collect::<Vec<(i32,i32)>>();
-///    
-///    println!("result: {:?}", xs); 
-/// # }
-/// ```
+/// You can use: 
+/// * ```Some( return_expresion)```  to return an expression value
+/// * ```v <- monadic_expression```  to use the monad result
+/// * ```_ <- monadic_expression```  to ignore the monad result
+/// * ```let z = expression```       to combine monad results
+/// * ```guard boolean_expression``` to filter results
+///
 #[macro_export]
 macro_rules! monadic {
   (let $v:ident = $e:expr ; $($rest:tt)*) => [Some($e).into_iter().flat_map( move |$v| { monadic!($($rest)*)} )];
