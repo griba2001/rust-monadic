cfe97b2ec68fa2d49cae4894fde44f5812f50b9b
Gabriel <griba2001@gmail.com>
2019-11-17 22:12:05 +0100

    trait Bind

--- src/monad.rs
+++ src/monad.rs
@@ -1,13 +1,26 @@
 // monad.rs
 
-use std::iter::{IntoIterator};
+use std::iter::{IntoIterator, FlatMap};
 use std::collections::{LinkedList, VecDeque};
 
 /// Monad<T> as supertrait of IntoIterator<Item=T>
 ///
 /// IntoIterator brings "into_iter().flat_map()" where its lazy result type FlatMap implements IntoIterator
 ///
-pub trait Monad<T>: IntoIterator<Item=T> { 
+pub trait Bind<T>: IntoIterator<Item=T> + Sized { 
+     
+     fn bind<U, F>(self, f: F) -> FlatMap<Self::IntoIter, U, F>
+        where 
+          F: FnMut(T) -> U,
+          U: IntoIterator,
+     {
+        self.into_iter().flat_map( f)
+     }
+   }
+   
+impl<T, R> Bind<T> for R where R: IntoIterator<Item=T> {}  
+
+pub trait Monad<T>: Bind<T> { 
 
      fn pure(x: T) -> Self;
    }
