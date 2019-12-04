7bec4bf6d610989d2204eae8cb9eccaf3bde07b2
Gabriel <griba2001@gmail.com>
2019-12-04 13:18:32 +0100

    update examples/state1

--- examples/state1.rs
+++ examples/state1.rs
@@ -1,7 +1,12 @@
+//! examples/state1.rs
+//!
+//! You may specify in a type restriction the type of the State bloc
+//! or apply it directly to an initial_state without the type restriction
+
 use monadic::{stdo, state::{State, get, put}};
 
 fn main() {
-  let res = stdo!{
+  let bloc: State<'_, i32, _> = stdo!{
   
        x <- pure 9;
        y <- get();
@@ -9,7 +14,9 @@ fn main() {
        z <- get(); 
        pure (x, y, z) 
        
-    }.initial_state( 0);
+    };
+    
+  let res = bloc.initial_state(0);  
 
   println!("result: {:?}", res);  
 }
