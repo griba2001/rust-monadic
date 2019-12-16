27fda2c91a40e650bba92994dcd64f1e19709678
Gabriel <griba2001@gmail.com>
2019-12-16 13:58:25 +0100

    examples

--- examples/writer_trans2.rs
+++ examples/writer_trans2.rs
@@ -1,28 +1,28 @@
-//! examples/writer_trans1.rs
+//! examples/writer_trans2.rs
 //!
 //! you may set the logger type 
 //! by beginning with a `tell...` function within the macro `wrdo` 
 //! or by declaring it as the result type 
 //! where String is the default if omitted
-//! as in `let res : WriterT< _, Log > = wrdo!{...}`
+//! as in `let res : WriterT< _, Vec<_>> = wrdo!{...}`
 //!
 //! `censor(), listen() and listens()` can be used as functions or as methods of a Writer bloc
 
 #[allow(unused_imports)]
 use monadic::{wrt_mdo, monad::Monad, writer_trans::{WriterT, tell, tell_str, tell_array, censor, listen}};
-use monadic::util::concat_string_str;
+use monadic::util::concat_vec_array;
 use partial_application::partial;
 use num::Integer;
 
-type Log = String;
+type Log = Vec<i32>;
 
 fn main() {
     
-    let modify_log = partial!( concat_string_str => _, "log2");
+    let modify_log = partial!( concat_vec_array => _, &[4,5,6]);
     
     let bloc = wrt_mdo!{  // : WriterT< Vec<_>, Log>
     
-        _ <- tell_str "log1" ;
+        _ <- tell_array &[1,2,3] ;
         
         x <- lift (5..9).collect::<Vec<_>>() ;
         
@@ -32,12 +32,12 @@ fn main() {
         // run a subbloc and modify its log afterwards
         pair <- censor( modify_log,
                         wrt_mdo!{
-                            _ <- tell_str "sub";
+                            _ <- tell_array &[0];
                             pure 2
                         }.listen()
                       );
                     
-        pure (z, pair.0, pair.1)
+        pure (z, pair.0, pair.1)            
         }.listen() ;
         
     // unwrap() returns the nested monad structure       
